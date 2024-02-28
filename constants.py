996cccba98d2758dc0e570054b6e2e153f30fa4e
Chaitanya Kundety <chaitanyakun@outlook.com>
2024-02-28 17:49:08 +0100

    update constants to read values from env variables

--- constants.py
+++ constants.py
@@ -1,2 +1,4 @@
-QDRANT_URL = 'localhost:6333'
-QDRANT_COLLECTION = 'advert_captions'
+import os
+
+QDRANT_URL = os.environ.get('QDRANT_URL', 'localhost:6333')
+QDRANT_COLLECTION = os.environ.get('QDRANT_COLLECTION', 'advert_captions')
